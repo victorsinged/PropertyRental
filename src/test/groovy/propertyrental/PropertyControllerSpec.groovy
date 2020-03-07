622a07683c5e7812ee2e37fbebfb009cf107de7d
Ale <victor.brito@vindi.com.br>
2020-03-07 14:58:06 -0300

    add controllers tests

---
+++ src/test/groovy/propertyrental/PropertyControllerSpec.groovy
@@ -0,0 +1,18 @@
+package propertyrental
+
+import grails.testing.web.controllers.ControllerUnitTest
+import spock.lang.Specification
+
+class PropertyControllerSpec extends Specification implements ControllerUnitTest<PropertyController> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
